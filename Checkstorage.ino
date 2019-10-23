cb955fc6af5dc6dda2dc489aec8752b47edd6b7e
JinsungGwag <jspartner1@naver.com>
2019-10-23 17:23:13 +0900

    Check distance by 4 ultrasonic waves

--- Checkstorage.ino
+++ Checkstorage.ino
@@ -6,22 +6,26 @@
 SoftwareSerial bluetooth(BT_RXD, BT_TXD);
 
 // LED PIN
-#define LED1 10
-#define LED2 9
-#define LED3 6
-#define LED4 5
+int ledPins[] = { 10, 9, 6, 5 };
+int pinCount = 4;
 
 // SENSOR PIN
 #define TRIG 4
-#define ECHO1 12
-#define ECHO2 11
-#define ECHO3 3
-#define ECHO4 2
+int echoPins[] = { 12, 11, 3, 2 };
+int echoCount = 4;
 
+int distance;
  
 void setup(){
   Serial.begin(9600);
   bluetooth.begin(9600);
+
+  pinMode(TRIG, OUTPUT);
+  for(int i = 0; i < pinCount; i++)
+  {
+    pinMode(ledPins[i], OUTPUT);
+    pinMode(echoPins[i], INPUT);
+  }  
 }
  
 void loop(){
@@ -31,4 +35,20 @@ void loop(){
   if (Serial.available()) {
     bluetooth.write(Serial.read());
   }
+
+  for(int i = 0; i < echoCount; i++)
+  {
+    digitalWrite(TRIG, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG, LOW);
+  
+    distance = pulseIn(echoPins[i], HIGH) / 58;
+    Serial.print("ECHO");
+    Serial.print(i + 1);
+    Serial.print(": ");
+    Serial.print(distance);
+    Serial.print("cm    ");
+  }
+  Serial.println();
+  delay(1000);
 }
