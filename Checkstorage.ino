14be5cbf39e8b08a87d66a71422b9ce46b83aff3
JinsungGwag <jspartner1@naver.com>
2019-10-23 16:18:33 +0900

    Set pin numbers

--- Checkstorage.ino
+++ Checkstorage.ino
@@ -1,8 +1,23 @@
 #include <SoftwareSerial.h>
- 
+
+// BLUETOOTH PIN
 #define BT_RXD 8
 #define BT_TXD 7
 SoftwareSerial bluetooth(BT_RXD, BT_TXD);
+
+// LED PIN
+#define LED1 10
+#define LED2 9
+#define LED3 6
+#define LED4 5
+
+// SENSOR PIN
+#define TRIG 4
+#define ECHO1 12
+#define ECHO2 11
+#define ECHO3 3
+#define ECHO4 2
+
  
 void setup(){
   Serial.begin(9600);
